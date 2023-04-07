cf42ef4d4dc62c58a10bc2a026e264262ec7b359
kevinvollmar <kevinvollmar@gmail.com>
2023-04-07 15:02:03 -0400

    Updated 2022_NFL_Passing.py

--- 2022_NFL_Passing.py
+++ 2022_NFL_Passing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Loading the 2022 NFL individual passing data into a Pandas dataframe
 passing_data = pd.read_csv('2022_NFL_PassingTable.csv')
@@ -7,11 +8,18 @@ passing_data = pd.read_csv('2022_NFL_PassingTable.csv')
 # Print the first 5 rows of df to check if data loaded correctly
 # print(passing_data.head())
 
+# Checking for missing values in the dataset
+# print(passing_data.isnull().sum())
+
+# Checking for outliers
+# plt.boxplot(passing_data['Yds'])
+# plt.show()
+
 # Basic statistics of the table
 # print(passing_data['Yds'].describe())
 
 # Scatter plot of touchdowns v. interceptions
-plt.scatter(passing_data['TD'], passing_data['Int'])
-plt.xlabel('Touchdowns')
-plt.ylabel('Interceptions')
-plt.show()
+# plt.scatter(passing_data['TD'], passing_data['Int'])
+# plt.xlabel('Touchdowns')
+# plt.ylabel('Interceptions')
+# plt.show()
