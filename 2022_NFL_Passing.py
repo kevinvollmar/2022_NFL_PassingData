2d9412528d3f9006d729fe22bcd699902e2e64ca
kevinvollmar <kevinvollmar@gmail.com>
2023-04-10 14:00:03 -0400

    Updated models

--- 2022_NFL_Passing.py
+++ 2022_NFL_Passing.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 # Loading the 2022 NFL individual passing data into a Pandas dataframe
 passing_data = pd.read_csv('2022_NFL_PassingTable.csv')
+
+#Drop rows with NaN values in any column
+passing_data = passing_data.dropna()
 
 # Print the first 5 rows of df to check if data loaded correctly
 # print(passing_data.head())
@@ -25,6 +31,20 @@ passing_data = pd.read_csv('2022_NFL_PassingTable.csv')
 # plt.show()
 
 # Seaborn data analysis looking to see if there is correlation between yards per attempt and sack %. Along with colorizing by age
-filtered_subset = passing_data.loc[passing_data['Att'] >= 100]
-sns.scatterplot(data=filtered_subset, x='Y/A', y='Sk%', hue='Age')
-plt.show()
+# filtered_subset = passing_data.loc[passing_data['Att'] >= 100]
+# sns.scatterplot(data=filtered_subset, x='Y/A', y='Sk%', hue='Age')
+# plt.show()
+
+#X = passing_data.drop(columns=['QBR'])
+X = passing_data[['Cmp','Att', 'TD','Int','Yds','Sk']]
+y = passing_data['QBR']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeRegressor()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print(predictions)
+
+mse = mean_squared_error(passing_data['QBR'], predictions)
+print("Mean squared error:", mse)
---
+++ 2022_NFL_PassingModel.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Loading the 2022 NFL individual passing data into a Pandas dataframe
+passing_data = pd.read_csv('2022_NFL_PassingTable.csv')
+
+# Drop rows with NaN values in any column
+passing_data = passing_data.dropna()
+
+X = passing_data[['TD','Int','Yds']]
+y = passing_data['QBR']
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeRegressor()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print(predictions)
+
+# Calculate R-squared
+r2 = r2_score(y_test, predictions)
+
+mse = mean_squared_error(y_test, predictions)
+print('Mean squared error:', mse)
+
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual QBR')
+plt.ylabel('Predicted QBR')
+plt.title('Actual v. Predicted QBR')
+
+# Add regression line with R-squared value to the plot
+sns.regplot(x=y_test, y=predictions, scatter=False, color='red',
+            line_kws={'label':"R-squared = {:.2f}".format(r2)})
+plt.legend(loc='upper left')
+
+plt.show()
